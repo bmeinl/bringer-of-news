1cfa7b424f4b5439db97aedcbd55ab1ce1261735
Benjamin Meinl <b.meinl@gmail.com>
2014-04-08 01:01:26 +0200

    Added automatic deletion of downvoted links.

--- bringer-of-news.py
+++ bringer-of-news.py
@@ -1,3 +1,4 @@
+from StringIO import StringIO
 import json
 import narwal
 import feedparser
@@ -5,6 +6,22 @@ import time
 import traceback
 from datetime import datetime
 import sys
+from collections import deque
+
+BOT_COMMENT = """
+This link was submitted by /u/%s, a bot to update a subreddit from various RSS feeds.  
+If this is a recent submission, **it will be removed once the score reaches %d**.
+For questions or suggestions, please contact one of the moderators listed on the sidebar.
+---
+Feel free to check out the source [on GitHub](https://github.com/bmeinl/bringer-of-news).
+"""
+
+
+
+# This is to temporarily inhibit stdout
+class NullIO(StringIO):
+    def write(self, txt):
+        pass
 
 def log(f, s):
     now = datetime.now()
@@ -23,9 +40,11 @@ def main(loghandle, config):
 
     last_checked = datetime.utcnow()
 
+    last_links = deque()
+
     while True:
         log(loghandle, "Checking for new feeds.")
-            
+
         for feed in config["feeds"]:
             d = feedparser.parse(feed)
             if d.bozo is 1:
@@ -34,17 +53,35 @@ def main(loghandle, config):
             for entry in d.entries:
                 # skip entries with empty fields, as that problem popped up recently
                 if not(entry.title) or not(entry.link):
-                        print "Skipping entry from '%s' because of empty fields." % feed
+                        log(loghandle, "Skipping entry from '%s' because of empty fields." % feed)
                         break
 
                 # if the entry is newer than when we last checked ...
                 if entry.published_parsed >= last_checked.timetuple():
                     try:
-                        link = session.submit_link(config["subreddit"], entry.title, entry.link, False)
-                        log(loghandle, "Posted \"%s\"" % link)
+                        link = session.submit_link(config["subreddit"], entry.title, entry.link)
+                        log(loghandle, "Posted \"%s\"" % link.title)
+                        last_links.append("t3_" + link.id)
+                        if len(last_links) > config["links_remembered"]: last_links.popleft()
+                        # we need this because the comment method has an ugly random print statement in it ...
+                        sys.stdout = NullIO()
+                        link.comment(BOT_COMMENT % (config["user_name"], config["deletion_score"]))
+                        sys.stdout = sys.__stdout__
                     except narwal.exceptions.PostError, e:
                         log(loghandle, "Couldn't submit link: %s (%s)" % (entry.title, e.errors))
-                        
+
+        log(loghandle, "Checking if any of the last %d links have a score of %d..." % (len(last_links),
+            config["deletion_score"]))
+
+        for id in list(last_links):
+            link = session.get("by_id", id)[0]
+            if link.score <= config["deletion_score"]:
+                data = dict(id=id)
+                res = session.post('api', 'del', data=data)
+                last_links.remove(id)
+                log(loghandle, "Deleted link: %s" % link.title)
+
+
         last_checked = datetime.utcnow()
         log(loghandle, "Sleeping %d second(s)." % config["pause_time"])
         time.sleep(config["pause_time"])
